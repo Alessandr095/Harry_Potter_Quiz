2f376b9ac978a8c4f98ab6044bd8c3b8ca6643bb
Alessandro De Siena <a.desiena@icloud.com>
2023-12-13 16:41:32 +0000

    Add indexes for the questions pull from question file.

--- run.py
+++ run.py
@@ -3,3 +3,10 @@ import random # for the use of randomising answers.
 from questions import questions # importing the questions from questions.py file for declutter.
 import logging as # logging module to prevent user errors within the quiz.
 
+qq = questions
+
+#indexes the questions from the questions file, instead of adding the answers by letter to the questions file, for randomisation of answers.
+total = len(qq)
+indexes = ['A', 'B', 'C', 'D']
+score = 0 
+q_num = 0
