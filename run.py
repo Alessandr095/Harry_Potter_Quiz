fddbb99fad8882d91c081a7e27740bab4efb3dba
Alessandro De Siena <a.desiena@icloud.com>
2023-12-13 17:20:35 +0000

    Add function to check if answers are correct or not according to letter indexes with use of the os module to make user interface more clear

--- run.py
+++ run.py
@@ -10,3 +10,28 @@ total = len(qq)
 indexes = ['A', 'B', 'C', 'D']
 score = 0 
 q_num = 0
+
+def check_answer(answers, correct, response):
+    """Function with if else statment to check if answer is correct or not according to the indexes"""
+    global score
+    global q_num
+
+    os.system('clear') # clears console for better user interface
+
+    pickedindex = indexes.index(response)
+    correctindex = answers.index(correct)
+
+    if pickedindex == correctindex:
+        os.system('clear')
+        print("Correct!")
+        score += 1
+    else:
+        print("Answer was incorrect")
+
+    input("Press enter to continue")
+
+    q_num += 1
+
+    qq.pop(0)
+
+    
